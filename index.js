00a92fe7810aea1384b0f7d80b719a29fdbe4240
Spencer <spencer.adler2@gmail.com>
2023-12-27 18:37:41 -0500

    complete Arithmetic lab

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+    return a+b
+  }
+function subtract(a,b) {
+    return a-b
+  }
+function multiply(a,b) {
+    return a*b
+  }
+function divide(a,b) {
+    return a/b
+  }
+function increment(n) {
+    n += 1 
+    return n
+  }
+function decrement(n) {
+    n -= 1
+    return n
+  }
+
+function makeInt(n) {
+    const t = parseInt(n, 10);
+    return t
+  }
+function preserveDecimal(n) {
+    const w = parseFloat(n, 10);
+    return w
+  }
